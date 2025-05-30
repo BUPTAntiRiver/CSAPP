a0112709a2a5efabb5543432bcbe8276f864ff98
Qianhe Xiao <lapis_xiao@outlook.com>
2025-05-30 18:09:45 +0800

    completed attack lab

---
+++ attacklab/target1/farm.s
@@ -0,0 +1,44 @@
+start_farm: f3 0f 1e fa b8 01 00 00 00 c3
+getval_142: f3 0f 1e fa b8 fb 78 90 90 c3
+addval_273: f3 0f 1e fa 8d 87 48 89 c7 c3 c3
+addval_219: f3 0f 1e fa 8d 87 51 73 58 90 c3
+setval_237: f3 0f 1e fa c7 07 48 89 c7 c7 c3
+setval_424: f3 0f 1e fa c7 07 54 c2 58 92 c3
+setval_470: f3 0f 1e fa c7 07 63 48 8d c7 c3
+setval_426: f3 0f 1e fa c7 07 48 89 c7 90 c3
+getval_280: f3 0f 1e fa b8 29 58 90 c3 c3
+mid_farm: f3 0f 1e fa b8 01 00 00 00 c3
+add_xy: f3 0f 1e fa 48 8d 04 37 c3
+getval_481: f3 0f 1e fa b8 5c 89 c2 90 c3
+setval_296: f3 0f 1e fa c7 07 99 d1 90 90 c3
+addval_113: f3 0f 1e fa 8d 87 89 ce 78 c9 c3
+addval_490: f3 0f 1e fa 8d 87 8d d1 20 db c3
+getval_226: f3 0f 1e fa b8 89 d1 48 c0 c3
+setval_384: f3 0f 1e fa c7 07 81 d1 84 c0 c3
+addval_190: f3 0f 1e fa 8d 87 41 48 89 e0 c3
+setval_276: f3 0f 1e fa c7 07 88 c2 08 c9 c3
+addval_436: f3 0f 1e fa 8d 87 89 ce 90 90 c3
+getval_345: f3 0f 1e fa b8 48 89 e0 c1 c3
+addval_479: f3 0f 1e fa 8d 87 89 c2 00 c9 c3
+addval_187: f3 0f 1e fa 8d 87 89 ce 38 c0 c3
+setval_248: f3 0f 1e fa c7 07 81 ce 08 db c3
+getval_159: f3 0f 1e fa b8 89 d1 38 c9 c3
+addval_110: f3 0f 1e fa 8d 87 c8 89 e0 c3 c3
+addval_487: f3 0f 1e fa 8d 87 89 c2 84 c0 c3
+addval_201: f3 0f 1e fa 8d 87 48 89 e0 c7 c3
+getval_272: f3 0f 1e fa b8 99 d1 08 d2 c3
+getval_155: f3 0f 1e fa b8 89 c2 c4 c9 c3
+setval_299: f3 0f 1e fa c7 07 48 89 e0 91 c3
+addval_404: f3 0f 1e fa 8d 87 89 ce 92 c3 c3
+getval_311: f3 0f 1e fa b8 89 d1 08 db c3
+setval_167: f3 0f 1e fa c7 07 89 d1 91 c3 c3
+setval_328: f3 0f 1e fa c7 07 81 c2 38 d2 c3
+setval_450: f3 0f 1e fa c7 07 09 ce 08 c9 c3
+addval_358: f3 0f 1e fa 8d 87 08 89 e0 90 c3
+addval_124: f3 0f 1e fa 8d 87 89 c2 c7 3c c3
+getval_169: f3 0f 1e fa b8 88 ce 20 c0 c3
+setval_181: f3 0f 1e fa c7 07 48 89 e0 c2 c3
+addval_184: f3 0f 1e fa 8d 87 89 c2 60 d2 c3
+getval_472: f3 0f 1e fa b8 8d ce 20 d2 c3
+setval_350: f3 0f 1e fa c7 07 48 89 e0 90 c3
+end_farm: f3 0f 1e fa b8 01 00 00 00 c3
